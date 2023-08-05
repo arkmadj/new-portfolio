986ad73f328eb0062f8ec0ead3ba6f5bf488c75b
arkmadj <ahmadjinadu@gmail.com>
2023-08-05 21:12:57 +0100

    added inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,12 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
