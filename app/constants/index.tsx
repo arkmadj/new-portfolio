aaa6a2f160d820522a66602f8f90ce5e6c73c2d1
arkmadj <ahmadjinadu@gmail.com>
2023-08-06 08:07:45 +0100

    added projects constant data

---
+++ app/constants/index.tsx
@@ -0,0 +1,26 @@
+import { ProjectCardProps } from "../components/ProjectCard";
+
+const projects: Array<ProjectCardProps> = [
+	{
+		name: "B54",
+    date: "January 2022 - July 2023",
+		paragraphs: [
+      "At B54, I collaborate with designers and product managers on the customer application to implement new features and improve existing ones based on user feedback and product requirements. In addition, I am responsible for implementing end-to-end unit testing, which has reduced regressions by approximately 90%. I was also part of the team that built the marketing website.",
+			"Tools I use to achieve this are Vue, NestJS, AdonisJS, NodeJS, Subsockets, Cypress, Quasar.",
+		],
+    link: "",
+    imgSrc: "",
+	},
+	{
+		name: "Shara",
+    date: "",
+		paragraphs: [
+      "I collaborated with a team of four engineers, one designer, and one product manager from the Flex team. Our goal was to provide a more versatile product with flexible terms. This upgrade allowed a broader range of customers to access our product, ensuring inclusivity and accessibility. As a result, we were able to reduce the churn rate by approximately 20%.",
+			"Tools I used to achieve this are ReactNative, TypeScript, Redis, AdonisJS, NodeJS, PostgresDB, MongoDB and JavaScript.",
+		],
+    link: "",
+    imgSrc: "",
+	},
+];
+
+export {projects}
