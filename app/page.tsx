a72b4e0afc44071f3edf598f83eeaf1eaac32b6b
arkmadj <ahmadjinadu@gmail.com>
2023-08-06 07:10:18 +0100

    added banner section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <NavBar/>
-      {/* <p>My Name is Ahmad</p> */}
-    </main>
-  )
+	return (
+		<main className="min-h-screen w-[1300px] ms-auto me-auto">
+			<NavBar />
+			<section className="min-h-[calc(100vh-152px)] bg-red-s00 flex items-end">
+				<h1 className=" font-mono text-[180px] font-extrabold">AHMAD JINADU</h1>
+			</section>
+		</main>
+	);
 }
