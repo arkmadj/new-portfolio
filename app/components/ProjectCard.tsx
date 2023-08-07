c3d2757070eeb41d9dc503b5f79b5d1c751409cf
arkmadj <ahmadjinadu@gmail.com>
2023-08-07 20:04:56 +0100

    added button icon

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import AppButton from "./AppButton";
+import Image from "next/image";
+import ArrowUpRight from "./svgs/ArrowUpRight";
 
 export interface ProjectCardProps {
 	name: string;
 	date: string;
 	paragraphs: string[];
 	link: string;
-	imgSrc: string;
+	imageComponent?: React.FC;
+	imgSrc?: string;
 }
 
 const ProjectCard = ({
@@ -13,20 +17,35 @@ const ProjectCard = ({
 	date,
 	paragraphs,
 	link,
+  imageComponent: ImageComponent,
 	imgSrc,
 }: ProjectCardProps) => {
 	return (
-		<section className="flex">
-			<div>
-				<h2>{name}</h2>
-				<p>{date}</p>
-				<div>
+		<section className="grid grid-cols-2 mb-10 border-2 border-white px-10 py-[60px]">
+			<div className="flex-grow">
+				<h2 className="mb-2 font-mono text-2xl font-bold">{name}</h2>
+				<p className="mb-8 font-sans text-lg font-medium">{date}</p>
+				<div className="flex flex-col gap-8 mb-[42px]">
 					{paragraphs.map((paragraph, key) => (
-						<p key={key}>{paragraph}</p>
+						<p className="font-sans text-lg" key={key}>
+							{paragraph}
+						</p>
 					))}
 				</div>
+				<AppButton label="Visit Project" icon={ArrowUpRight}/>
 			</div>
-			<div></div>
+			<div className="flex justify-end flex-grow">
+        {ImageComponent && <ImageComponent/>}
+				{/* {imgSrc && (
+					<Image
+						src={imgSrc}
+						alt="B54 application dashboard"
+						width={600}
+						height={600}
+					/>
+				)} */}
+				{/* {imageComponent && <Image} */}
+			</div>
 		</section>
 	);
 };
