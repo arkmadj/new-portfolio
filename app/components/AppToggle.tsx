3774f1bfa5e540212e869573f36a650a0681f0e3
arkmadj <ahmadjinadu@gmail.com>
2023-08-07 23:50:43 +0100

    added class prop

--- app/components/AppToggle.tsx
+++ app/components/AppToggle.tsx
@@ -1,17 +1,18 @@
-'use client'
+"use client";
 
 import React from "react";
 
 type AppToggleProps = {
 	onChange: () => void;
+	className?: string;
 };
 
-const AppToggle = ({onChange }: AppToggleProps) => {
+const AppToggle = ({ onChange, className }: AppToggleProps) => {
 	return (
 		<input
 			type="checkbox"
-      onChange={onChange}
-			className="relative shadow-lg outline-none appearance-none cursor-pointer w-[35px] h-5 border-2 border-white rounded-3xl before:transition-all before:duration-500 motion-reduce:before:transition-none before:w-5 before:h-5 before:bg-white before:absolute before:rounded-full before:-top-[2px] before:-right-[1px] checked:before:transform checked:before:-translate-x-3.5"
+			onChange={onChange}
+			className={`relative outline-none appearance-none cursor-pointer w-[35px] h-5 border-2 border-foreground rounded-3xl before:transition-all before:duration-500 motion-reduce:before:transition-none before:w-5 before:h-5 before:bg-foreground before:absolute before:rounded-full before:-top-[2px] before:-right-[1px] checked:before:transform checked:before:-translate-x-3.5 ${className}`}
 		/>
 	);
 };
