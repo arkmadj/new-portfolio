938807ffee18dec840f2c968b719937c8bc6de56
arkmadj <ahmadjinadu@gmail.com>
2023-08-07 20:04:35 +0100

    added darkmode toggle

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,29 @@
+"use client";
+
 import React from "react";
 import AppToggle from "./AppToggle";
 
 const NavBar = () => {
+	// const [isDarkMode, setIsDarkMode] = useState(false)
+
+	// setIsDarkMode(!isDarkMode)
+
+	const toggleDarkMode = () => {
+		document.body.classList.toggle("dark")
+	};
+
+	const addDarkMode = () => {
+		const body = document.body;
+		body.classList.remove("light-mode");
+		body.classList.add("dark-mode");
+	};
+
+	const addLightMode = () => {
+		const body = document.body;
+		body.classList.remove("dark-mode");
+		body.classList.add("light-mode");
+	};
+
 	const navLinks = [
 		{
 			name: "Resume",
@@ -25,14 +47,14 @@ const NavBar = () => {
 					<p>Based in Lagos, Nigeria 09:23AM</p>
 				</div>
 			</div>
-			<ul className="flex list-none gap-[50px] items-center">
+			<ul className="flex list-none gap-[50px]">
 				{navLinks.map((link, key) => (
 					<li key={key} className="cursor-pointer">
 						{link.name}
 					</li>
 				))}
 				<li>
-					<AppToggle />
+					<AppToggle onChange={toggleDarkMode} />
 				</li>
 			</ul>
 		</nav>
