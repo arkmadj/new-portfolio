2236a19ad1b0a0e3432c61fed7346876c3ce4cd9
arkmadj <ahmadjinadu@gmail.com>
2023-08-07 19:44:58 +0100

    added toggle functionality

--- app/components/AppToggle.tsx
+++ app/components/AppToggle.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+'use client'
 
-const AppToggle = () => {
-  return (
-    <div className='bg-white w-[35px] h-5 rounded-full'></div>
-  )
-}
+import React from "react";
 
-export default AppToggle
+type AppToggleProps = {
+	onChange: () => void;
+};
+
+const AppToggle = ({onChange }: AppToggleProps) => {
+	return (
+		<input
+			type="checkbox"
+      onChange={onChange}
+			className="relative shadow-lg outline-none appearance-none cursor-pointer w-[35px] h-5 border-2 border-white rounded-3xl before:transition-all before:duration-500 motion-reduce:before:transition-none before:w-5 before:h-5 before:bg-white before:absolute before:rounded-full before:-top-[2px] before:-right-[1px] checked:before:transform checked:before:-translate-x-3.5"
+		/>
+	);
+};
+
+export default AppToggle;
