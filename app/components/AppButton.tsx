76c5b0f837ccad0ae292c5266bdbdcbc166ac473
arkmadj <ahmadjinadu@gmail.com>
2023-08-09 21:24:34 +0100

    added left and right icon props to button

--- app/components/AppButton.tsx
+++ app/components/AppButton.tsx
@@ -1,19 +1,28 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export interface ButtonProps {
-  label: string;
-  icon?: React.FC;
-  className?: string
+	label: string;
+	rightIcon?: React.FC;
+	leftIcon?: React.FC;
+	className?: string;
 }
 
-const AppButton = ({label, icon: Icon, className}: ButtonProps) => {
-  return (
-    <button className={`flex gap-1 items-center font-sans text-black bg-white border-none outline-none px-6 py-3 rounded font-bold ${className}`}>
-      {label}
-      {Icon && <Icon/>}
-      </button>
-  )
-}
+const AppButton = ({
+	label,
+	rightIcon: RightIcon,
+	leftIcon: LeftIcon,
+	className,
+}: ButtonProps) => {
+	return (
+		<button
+			className={`flex gap-1 items-center font-sans text-black border-none outline-none px-6 py-3 rounded font-bold ${className}`}
+		>
+			{LeftIcon && <LeftIcon />}
+			{label}
+			{RightIcon && <RightIcon />}
+		</button>
+	);
+};
 
-export default AppButton
+export default AppButton;
