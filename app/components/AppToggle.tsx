d9fdcea961fd92ed2b57ac97d4e8d8e6d38b734b
arkmadj <ahmadjinadu@gmail.com>
2023-08-10 16:11:59 +0100

    added transition on transforms only

--- app/components/AppToggle.tsx
+++ app/components/AppToggle.tsx
@@ -12,7 +12,7 @@ const AppToggle = ({ onChange, className }: AppToggleProps) => {
 		<input
 			type="checkbox"
 			onChange={onChange}
-			className={`relative outline-none appearance-none cursor-pointer w-[35px] h-5 border-2 border-foreground rounded-3xl before:transition-all before:duration-500 motion-reduce:before:transition-none before:w-5 before:h-5 before:bg-foreground before:absolute before:rounded-full before:-top-[2px] before:-right-[1px] checked:before:transform checked:before:-translate-x-3.5 ${className}`}
+			className={`relative outline-none appearance-none cursor-pointer w-[35px] h-5 border-2 border-foreground rounded-3xl before:transition-transform before:duration-500 motion-reduce:before:transition-none before:w-5 before:h-5 before:bg-foreground before:absolute before:rounded-full before:-top-[2px] before:-right-[1px] checked:before:transform checked:before:-translate-x-3.5 ${className}`}
 		/>
 	);
 };
