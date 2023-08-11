fd22aab8a46419db2035f2a4d42244085fa3042d
arkmadj <ahmadjinadu@gmail.com>
2023-08-11 16:04:07 +0100

    change to jsx attribute names

--- app/components/svgs/EyeIcon.tsx
+++ app/components/svgs/EyeIcon.tsx
@@ -16,8 +16,8 @@ const EyeIcon = ({ className }: EyeIconProps) => {
 			>
 				<path
 					d="M8.14449 1.23195C3.47782 1.23195 0.811157 6.56528 0.811157 6.56528C0.811157 6.56528 3.47782 11.8986 8.14449 11.8986C12.8112 11.8986 15.4778 6.56528 15.4778 6.56528C15.4778 6.56528 12.8112 1.23195 8.14449 1.23195Z"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 			</svg>
 		</div>
